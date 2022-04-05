fb60a5cccdf034623df7fc397f98efa1a74633a8
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-04-05 22:24:22 +0700

    make features component on register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,8 +1,8 @@
 <script setup>
 import RegisterForm from "@/components/authentication/RegisterForm.vue";
-import IconBadge from "@/components/icons/IconBadge.vue";
-import IconBook from "@/components/icons/IconBook.vue";
-import IconLayer from "@/components/icons/IconLayer.vue";
+import Customizable from "@/components/authentication/features/Customizable.vue";
+import Documentation from "@/components/authentication/features/Documentation.vue";
+import Fortune from "@/components/authentication/features/Fortune.vue";
 </script>
 
 <template>
@@ -28,42 +28,9 @@ import IconLayer from "@/components/icons/IconLayer.vue";
           <div class="w-full p-5 mx-auto md:max-w-4xl">
             <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
               <div class="mx-auto md:mx-0">
-                <div class="mb-12">
-                  <div class="block mb-2">
-                    <IconLayer/>
-                    <h3 class="inline-block ml-2 text-lg font-semibold">
-                      Customizable
-                    </h3>
-                  </div>
-                  <p class="mt-2 text-lg text-gray-500">
-                    Accelerate your work with our millions <br />
-                    of ready-to-use design projects.
-                  </p>
-                </div>
-                <div class="mb-12">
-                  <div class="block mb-2">
-                    <IconBadge/>
-                    <h3 class="inline-block ml-2 text-lg font-semibold">
-                      500 Fortune
-                    </h3>
-                  </div>
-                  <p class="mt-2 text-lg text-gray-500">
-                    Accelerate your work with our millions <br />
-                    of ready-to-use design projects.
-                  </p>
-                </div>
-                <div class="mb-12">
-                  <div class="block mb-2">
-                    <IconBook/>
-                    <h3 class="inline-block ml-2 text-lg font-semibold">
-                      Documentation
-                    </h3>
-                  </div>
-                  <p class="mt-2 text-lg text-gray-500">
-                    Accelerate your work with our millions <br />
-                    of ready-to-use design projects.
-                  </p>
-                </div>
+                <Documentation/>
+                <Fortune/>
+                <Customizable/>
               </div>
               <div>
                 <RegisterForm />
