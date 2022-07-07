9b6991c0781057bb56aa30545ab5962c829929d1
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-07 19:10:38 +0700

    day 10 : add flowbite & fixe dropdown profile

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,6 +1,12 @@
 <script setup>
   import Gallery from '../components/detail/Gallery.vue'
   import { RouterLink  } from 'vue-router';
+  import { onMounted } from 'vue';
+
+  onMounted(() => {
+    window.scrollTo(0,0);
+  })
+
 </script>
 
 <template>
