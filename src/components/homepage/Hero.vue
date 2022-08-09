f63c75931477bd727cb0df49d1985f9a00b7a7dd
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-08-09 23:03:27 +0700

    install prettier tailwind

--- src/components/homepage/Hero.vue
+++ src/components/homepage/Hero.vue
@@ -3,7 +3,7 @@
   <div class="relative overflow-hidden bg-white">
     <div class="mx-auto max-w-7xl">
       <div
-        class="z-10 px-5 pb-8 bg-white xl:pl-20 lg:pl-16 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+        class="z-10 bg-white px-5 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pl-16 lg:pb-28 xl:pl-20 xl:pb-32"
       >
         <main
           class="mx-auto mt-10 max-w-7xl sm:mt-12 md:mt-16 lg:mt-20 xl:mt-36"
@@ -16,7 +16,7 @@
               <span class="block">Faster Than Before.</span>
             </h1>
             <p
-              class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+              class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0"
             >
               Accelerate your work with our millions
               <span class="block lg:mb-1"></span> of ready-to-use design
@@ -28,7 +28,7 @@
               <div class="rounded-full">
                 <a
                   href="#categories"
-                  class="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-lg md:px-10 hover:shadow"
+                  class="flex w-full items-center justify-center rounded-full border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 hover:shadow md:py-2 md:px-10 md:text-lg"
                   >Browse Now</a
                 >
               </div>
@@ -39,7 +39,7 @@
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
-        class="w-full lg:w-3/4 lg:mt-8"
+        class="w-full lg:mt-8 lg:w-3/4"
         src="@/assets/img/hero-image.png"
         alt=""
       />
